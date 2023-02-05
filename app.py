97707f22aacbc9a57d0c137086a01ca23dcbc28b
argyaWicaksana <argya7@gmail.com>
2023-02-05 15:24:38 +0700

    specify cors

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ DB_NAME =  os.environ.get("DB_NAME")
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
 app = Flask(__name__)
-# cors = CORS(app, resources={r"/*": {"origins": "*"}})
-CORS(app)
+CORS(app, origins="http://localhost:5173")
 
 def parse_json(data):
     data = json.loads(json_util.dumps(data))
@@ -62,4 +61,4 @@ def homework_delete():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('localhost', port=5000, debug=True)
